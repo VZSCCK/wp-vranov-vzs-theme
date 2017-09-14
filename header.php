2d6684a79d90aa8b13c7e03b76c462e7a2468e59
irkallacz <mottl.jakub.janov@gmail.com>
2017-09-14 21:56:24 +0200

    Menu reformat

--- header.php
+++ header.php
@@ -1,41 +1,42 @@
 <!DOCTYPE html>
 <html lang="cs">
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta http-equiv="cleartype" content="on">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="<?php bloginfo('stylesheet_url'); ?>" media="screen" rel="stylesheet" type="text/css">
-  <script src="<?php echo BASE_URL; ?>/js/jquery-1.11.min.js"></script>
-  <link rel="shortcut icon" type="image/x-icon" href="<?php echo BASE_URL; ?>/favicon.png">
-  <title><?php wp_title( '|', true, 'right' ); bloginfo('blockname'); ?></title>
-  <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="cleartype" content="on">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="<?php bloginfo('stylesheet_url'); ?>" media="screen" rel="stylesheet" type="text/css">
+	<script src="<?php echo BASE_URL; ?>/js/jquery-1.11.min.js"></script>
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo BASE_URL; ?>/favicon.ico">
+	<link rel="icon" type="image/svg+xml" href="<?php echo BASE_URL; ?>/favicon.svg" sizes="any">
+	<title><?php wp_title( '|', true, 'right' ); bloginfo('blockname'); ?></title>
+	<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
 
-  <!-- HEADER -->
-  <div class="header">
-    <nav class="topmenu"">
-      <ul>
-        <li><a id="menu-button">☰ Menu</a></li>
-      </ul>
-      <?php wp_nav_menu(['menu_id' => 'menu-top', 'container' => '', 'theme_location'=>'top']);?> 
-    </nav>
+	<!-- HEADER -->
+	<div class="header">
+	<nav class="topmenu"">
+	  <ul>
+		<li><a id="menu-button">☰ Menu</a></li>
+	  </ul>
+	  <?php wp_nav_menu(['menu_id' => 'menu-top', 'container' => '', 'theme_location'=>'top']);?>
+	</nav>
 
-    <div class="line white">
-      <div class="wrap">
-        <a href="<?php bloginfo('url')?>" class="logo"><img src="<?php echo BASE_URL; ?>/images/logo.svg"></a>
-        <strong class="supertitle">Vodní záchranná služba ČČK</strong>
-      </div>
-    </div>
-      
-    <div class="line blue">
-      <div class="wrap">
-      <strong class="subtitle">Jablonec nad Nisou</strong>
-    </div>
-    </div>
-  </div>
-  <!-- / HEADER -->
+	<div class="line white">
+	  <div class="wrap">
+		<a href="<?php bloginfo('url')?>" class="logo"><img src="<?php echo BASE_URL; ?>/images/logo.svg"></a>
+		<strong class="supertitle">Vodní záchranná služba ČČK</strong>
+	  </div>
+	</div>
+
+	<div class="line blue">
+	  <div class="wrap">
+	  <strong class="subtitle">Jablonec nad Nisou</strong>
+	</div>
+	</div>
+	</div>
+	<!-- / HEADER -->
 
   <!-- PAGE -->
   <div class="content">

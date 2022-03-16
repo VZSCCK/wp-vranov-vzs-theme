8ed3b90334520f50c59161e3e2dc96b5779f45e1
behounek <tomas@behounek.cz>
2022-03-16 08:48:04 +0100

    Zmena nazvu PS

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 
 		<div class="line blue">
 			<div class="wrap">
-				<strong class="subtitle">Jablonec nad Nisou</strong>
+				<strong class="subtitle">Vranov</strong>
 			</div>
 		</div>
 	</div>
